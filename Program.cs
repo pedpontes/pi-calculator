def432fdb245b56255664f95394f9d6f59e40ac7
unknown <pontespeter@gmail.com>
2024-02-20 23:02:06 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace PI;
+public class Pi {
+
+    static void Main(string[] argv){
+        double s = 0.0;
+        double pi;
+        double divisor = 1.0;
+
+        for(double counter = 1; counter <= 100; counter++){
+            s = counter % 2 == 0
+                ? s - Math.Pow(1/divisor, 3.0) 
+                : s + Math.Pow(1/divisor, 3.0);
+            divisor += 2;
+        }
+        pi = Math.Pow(s*32, 1.000/3.000);
+
+        Console.WriteLine($"\n\n\tPI: {pi},\n\tSérie: {s}\n\n");
+        Console.ReadLine();
+    }
+}
